bd6c4bb792840ba7fcd80f38280fbfb0bda57064
michelszklo <mszklo@gmail.com>
2022-04-25 18:15:55 -0300

    implements loop for all IVs

--- RUN_ALL_REGS.R
+++ RUN_ALL_REGS.R
@@ -53,61 +53,84 @@ dir <- "C:/Users/Michel/Google Drive/DOUTORADO FGV/Artigos/EC 29-2000/"
 # 1. Folder, files and instrument setup
 # =================================================================
 
-# Regressions' outputs main folder
-# ------------------------------------
-main_folder <- "regs_outputs/"
-
-# Reduced form yearly estimates figures folder
-# ------------------------------------
-yearly_folder <- "yearly_reduced_form/"
-
-# Instrumental variable (uncoment the selected one)
-# ------------------------------------
-
-instrument <- "ec29_baseline"
-# instrument <- "ec29_baseline_below"
-# instrument <- "dist_ec29_baseline"
-# instrument <- "dist_ec29_baseline_below"
-# instrument <- "dist_spending_pc_baseline"
-# instrument <- "dist_spending_pc_baseline_below"
-
-# is the instrument restricted to the sample below the target?
-below <- 0
-
-# Regression output excel file
-# ------------------------------------
-output_file <- "results_ec29_baseline.xlsx"
+running_map <- rbind(
+  cbind("regs_outputs/","yearly_reduced_form_ec29/","ec29_baseline",0,"results_ec29_baseline.xlsx"),
+  cbind("regs_outputs/","yearly_reduced_form_ec29_b/","ec29_baseline_below",1,"results_ec29_baseline.xlsx"),
+  cbind("regs_outputs/","yearly_reduced_form_dist_ec29/","dist_ec29_baseline",0,"results_dist_ec29_baseline.xlsx"),
+  cbind("regs_outputs/","yearly_reduced_form_dist_ec29_b/","dist_ec29_baseline_below",1,"results_dist_ec29_baseline.xlsx"),
+  cbind("regs_outputs/","yearly_reduced_form_dist_spending/","dist_spending_pc_baseline",0,"results_dist_ec29_baseline.xlsx"),
+  cbind("regs_outputs/","yearly_reduced_form_dist_spending_b/","dist_spending_pc_baseline_below",1,"results_dist_spending_baseline.xlsx")
+)
 
 
-save.image(paste0(dir,"output_setup.RData"))
-
-
-
-# 2. Run all
+# 2. Run all loop
 # =================================================================
 
-scripts <- "ec29/"
-source(paste0(dir,scripts,"08_regs_vars_specs.R"))
-scripts <- "ec29/"
-source(paste0(dir,scripts,"09_regs_firststage.R"))
-scripts <- "ec29/"
-source(paste0(dir,scripts,"10_regs_spending.R"))
-scripts <- "ec29/"
-source(paste0(dir,scripts,"11_regs_infra.R"))
-scripts <- "ec29/"
-source(paste0(dir,scripts,"12_regs_sia.R"))
-scripts <- "ec29/"
-source(paste0(dir,scripts,"13_regs_hr.R"))
-scripts <- "ec29/"
-source(paste0(dir,scripts,"14_regs_imr.R"))
-scripts <- "ec29/"
-source(paste0(dir,scripts,"15_regs_amr.R"))
-scripts <- "ec29/"
-source(paste0(dir,scripts,"16_regs_imr_lag.R"))
-scripts <- "ec29/"
-source(paste0(dir,scripts,"17_regs_amr_lag.R"))
-scripts <- "ec29/"
-source(paste0(dir,scripts,"18_regs_birth.R"))
-
-
-
+for(i in seq.int(1:nrow(running_map))){
+  
+  # Regressions' outputs main folder
+  # ------------------------------------
+  main_folder <- running_map[i,1]
+  print(paste0("Main folder: ", main_folder))
+  
+  # Reduced form yearly estimates figures folder
+  # ------------------------------------
+  yearly_folder <- running_map[i,2]
+  print(paste0("RF yearly graphs folder: ", yearly_folder))
+  
+  # Instrumental variable (uncoment the selected one)
+  # ------------------------------------
+  instrument <- running_map[i,3]
+  print(paste0("Instrumental Variable: ", instrument))
+  
+  # is the instrument restricted to the sample below the target?
+  below <- running_map[i,4]
+  if(below==0){
+    p <- "No"
+  } else {
+    p <- "Yes"
+  }
+  print(paste0("Instrument variation restricted to below target sample? ", p))
+  
+  # Regression output excel file
+  # ------------------------------------
+  output_file <- running_map[i,5]
+  print(paste0("Output file name: ", output_file))
+  
+  print("")
+  print("")
+  print("")
+  
+  # Saving set up
+  # ------------------------------------
+  save.image(paste0(dir,"output_setup.RData"))
+  
+  
+  # Run all regressions
+  # ------------------------------------
+  
+  scripts <- "ec29/"
+  source(paste0(dir,scripts,"08_regs_vars_specs.R"))
+  scripts <- "ec29/"
+  source(paste0(dir,scripts,"09_regs_firststage.R"))
+  scripts <- "ec29/"
+  source(paste0(dir,scripts,"10_regs_spending.R"))
+  scripts <- "ec29/"
+  source(paste0(dir,scripts,"11_regs_infra.R"))
+  scripts <- "ec29/"
+  source(paste0(dir,scripts,"12_regs_sia.R"))
+  scripts <- "ec29/"
+  source(paste0(dir,scripts,"14_regs_imr.R"))
+  scripts <- "ec29/"
+  source(paste0(dir,scripts,"15_regs_amr.R"))
+  scripts <- "ec29/"
+  # source(paste0(dir,scripts,"16_regs_imr_lag.R"))
+  # scripts <- "ec29/"
+  # source(paste0(dir,scripts,"17_regs_amr_lag.R"))
+  scripts <- "ec29/"
+  source(paste0(dir,scripts,"18_regs_birth.R"))
+  scripts <- "ec29/"
+  source(paste0(dir,scripts,"19_regs_cnes.R"))
+  
+  
+}
