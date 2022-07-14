541bde36101f249ad070c5ba6d7020c6d8badc3e
michelszklo <mszklo@gmail.com>
2022-07-14 17:28:27 -0300

    removes by type of worker

--- 12_regs_sia.R
+++ 12_regs_sia.R
@@ -58,86 +58,86 @@ load(paste0(dir,"regs.RData"))
 var_map <- rbind(cbind('sia_pcapita','N. Outpatient Procedures (per capita)'),
                  cbind('sia_ab_pcapita','N. Primary Care Outpatient Procedures (per capita)'),
                  cbind('sia_nprod_amb_lc_mun_pcapita','N. Low & Mid Complexity Outpatient Procedures (per capita)'),
-                 cbind('sia_nprod_amb_hc_mun_pcapita','N. High Complexity Outpatient Procedures (per capita)'),
-                 cbind('sia_nprod_low_skill_mun_pcapita','N. Outpatient Procedures by Low Skilled Workers (per capita)'),
-                 cbind('sia_nprod_med_skill_mun_pcapita','N. Outpatient procedures by Mid Skilled Workers (per capita)')
+                 cbind('sia_nprod_amb_hc_mun_pcapita','N. High Complexity Outpatient Procedures (per capita)')
+                 # cbind('sia_nprod_low_skill_mun_pcapita','N. Outpatient Procedures by Low Skilled Workers (per capita)'),
+                 # cbind('sia_nprod_med_skill_mun_pcapita','N. Outpatient procedures by Mid Skilled Workers (per capita)')
                  
 )
 
-                 
-                 
-                 
-                 
-                 
 
-                 # cbind('sia_nprod_amb_lc_pcapita','Low & Mid Complexity Outpatient Procedures (per capita)'),
-                 # cbind('sia_nprod_amb_lc_pub_pcapita','Public Low & Mid Complexity Outpatient Procedures (per capita)'),
-                 # cbind('sia_nprod_amb_lc_pvt_pcapita','Private Low & Mid Complexity Outpatient Procedures (per capita)'),
-                 # 
-                 # cbind('sia_nprod_amb_hc_pcapita','High Complexity Outpatient Procedures (per capita)'),
-                 # cbind('sia_nprod_amb_hc_pub_pcapita','Public High Complexity Outpatient Procedures (per capita)'),
-                 # cbind('sia_nprod_amb_hc_pvt_pcapita','Private High Complexity Outpatient Procedures (per capita)'),
-                 # 
-                 # cbind('sia_nprod_low_skill_pcapita','Outpatient Procedures by Low Skilled Workers (per capita)'),
-                 # cbind('sia_nprod_low_skill_pub_pcapita','Public Outpatient Procedures by Low Skilled Workers (per capita)'),
-                 # cbind('sia_nprod_low_skill_pvt_pcapita','Private Outpatient Procedures by Low Skilled Workers (per capita)'),
-                 # 
-                 # cbind('sia_nprod_med_skill_pcapita','Outpatient Procedures by Mid Skilled Workers (per capita)'),
-                 # cbind('sia_nprod_med_skill_pub_pcapita','Public Outpatient procedures by Mid Skilled Workers (per capita)'),
-                 # cbind('sia_nprod_med_skill_pvt_pcapita','Private Outpatient procedures by Mid Skilled Workers (per capita)'),
-                 # 
-                 # cbind('sia_nprod_high_skill_pcapita','Outpatient Procedures by High Skilled Workers (per capita)'),
-                 # cbind('sia_nprod_high_skill_mun_pcapita','Municipal Outpatient Procedures by High Skilled Workers (per capita)'),
-                 # cbind('sia_nprod_high_skill_pub_pcapita','Public Outpatient Procedures by High Skilled Workers (per capita)'),
-                 # cbind('sia_nprod_high_skill_pvt_pcapita','Private Outpatient Procedures by High Skilled Workers (per capita)'),
-                 # 
-                 # cbind('sia_ncnes_amb_pcapita','N. of Health Facilities with Ambulatory Service (per capita*1000)'),
-                 # cbind('sia_ncnes_amb_pub_pcapita','N. of Public Health Facilities with Ambulatory Service (per capita*1000)'),
-                 # cbind('sia_ncnes_amb_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service (per capita*1000)'),
-                 # 
-                 # cbind('sia_ncnes_amb_lc_pcapita','N. of Health Facilities with Low & Mid Complexity Ambulatory Service (per capita*1000)'),
-                 # cbind('sia_ncnes_amb_lc_pub_pcapita','N. of Public Health Facilities with Low & Mid Complexity Ambulatory Service (per capita*1000)'),
-                 # cbind('sia_ncnes_amb_lc_pvt_pcapita','N. of Private Health Facilities with Low & Mid Complexity Ambulatory Service (per capita*1000)'),
-                 # 
-                 # cbind('sia_ncnes_amb_hc_pcapita','N. of Health Facilities with High Complexity Ambulatory Service (per capita*1000)'),
-                 # cbind('sia_ncnes_amb_hc_pub_pcapita','N. of Public Health Facilities with High Complexity Ambulatory Service (per capita*1000)'),
-                 # cbind('sia_ncnes_amb_hc_pvt_pcapita','N. of Private Health Facilities with High Complexity Ambulatory Service (per capita*1000)'),
-                 # 
-                 # 
-                 # cbind('sia_ncnes_low_skill_pcapita','N. of Health Facilities with Ambulatory Service by Low Skilled Workers (per capita*1000)'),
-                 # cbind('sia_ncnes_low_skill_pub_pcapita','N. of Public Health Facilities with Ambulatory Service by Low Skilled Workers (per capita*1000)'),
-                 # cbind('sia_ncnes_low_skill_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service by Low Skilled Workers (per capita*1000)'),
-                 # 
-                 # cbind('sia_ncnes_med_skill_pcapita','N. of Health Facilities with Ambulatory Service by Mid Skilled Workers (per capita*1000)'),
-                 # cbind('sia_ncnes_med_skill_pub_pcapita','N. of Public Health Facilities with Ambulatory Service by Mid Skilled Workers (per capita*1000)'),
-                 # cbind('sia_ncnes_med_skill_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service by Mid Skilled Workers (per capita*1000)'),
-                 # 
-                 # cbind('sia_ncnes_high_skill_pcapita','N. of Health Facilities with Ambulatory Service by High Skilled Workers (per capita*1000)'),
-                 # cbind('sia_ncnes_high_skill_mun_pcapita','N. of Municipal Health Facilities with Ambulatory Service by High Skilled Workers (per capita*1000)'),
-                 # cbind('sia_ncnes_high_skill_pub_pcapita','N. of Public Health Facilities with Ambulatory Service by High Skilled Workers (per capita*1000)'),
-                 # cbind('sia_ncnes_high_skill_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service by High Skilled Workers (per capita*1000)'),
-                 # 
-                 # cbind('sia_ncnes_enf_pcapita','N. of Health Facilities with Ambulatory Service by Nurses (per capita*1000)'),
-                 # cbind('sia_ncnes_enf_pub_pcapita','N. of Public Health Facilities with Ambulatory Service and with Nurses (per capita*1000)'),
-                 # cbind('sia_ncnes_enf_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service and with Nurses (per capita*1000)'),
-                 # 
-                 # cbind('sia_ncnes_enfobs_pcapita','N. of Health Facilities with Ambulatory Service by Obstetrical Nurses (per capita*1000)'),
-                 # cbind('sia_ncnes_enfobs_pub_pcapita','N. of Public Health Facilities with Ambulatory Service by Obstetrical Nurses (per capita*1000)'),
-                 # cbind('sia_ncnes_enfobs_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service by Obstetrical Nurses (per capita*1000)'),
-                 # 
-                 # cbind('sia_ncnes_ginobs_pcapita','N. of Health Facilities with Obstetrical/Gyneco. Ambulatory Service (per capita*1000)'),
-                 # cbind('sia_ncnes_ginobs_pub_pcapita','N. of Public Health Facilities with Obstetrical/Gyneco. Ambulatory Service (per capita*1000)'),
-                 # cbind('sia_ncnes_ginobs_pvt_pcapita','N. of Private Health Facilities with Obstetrical/Gyneco. Ambulatory Service (per capita*1000)'),
-                 # 
-                 # cbind('sia_ncnes_pediat_pcapita','N. of Health Facilities with Pediatric Ambulatory Service (per capita*1000)'),
-                 # cbind('sia_ncnes_pediat_pub_pcapita','N. of Public Health Facilities with Pediatric Ambulatory Service (per capita*1000)'),
-                 # cbind('sia_ncnes_pediat_pvt_pcapita','N. of Private Health Facilities with Pediatric Ambulatory Service (per capita*1000)'),
-                 # 
-                 # 
-                 # 
-                 # 
-                 # 
-                 # )
+
+
+
+
+
+# cbind('sia_nprod_amb_lc_pcapita','Low & Mid Complexity Outpatient Procedures (per capita)'),
+# cbind('sia_nprod_amb_lc_pub_pcapita','Public Low & Mid Complexity Outpatient Procedures (per capita)'),
+# cbind('sia_nprod_amb_lc_pvt_pcapita','Private Low & Mid Complexity Outpatient Procedures (per capita)'),
+# 
+# cbind('sia_nprod_amb_hc_pcapita','High Complexity Outpatient Procedures (per capita)'),
+# cbind('sia_nprod_amb_hc_pub_pcapita','Public High Complexity Outpatient Procedures (per capita)'),
+# cbind('sia_nprod_amb_hc_pvt_pcapita','Private High Complexity Outpatient Procedures (per capita)'),
+# 
+# cbind('sia_nprod_low_skill_pcapita','Outpatient Procedures by Low Skilled Workers (per capita)'),
+# cbind('sia_nprod_low_skill_pub_pcapita','Public Outpatient Procedures by Low Skilled Workers (per capita)'),
+# cbind('sia_nprod_low_skill_pvt_pcapita','Private Outpatient Procedures by Low Skilled Workers (per capita)'),
+# 
+# cbind('sia_nprod_med_skill_pcapita','Outpatient Procedures by Mid Skilled Workers (per capita)'),
+# cbind('sia_nprod_med_skill_pub_pcapita','Public Outpatient procedures by Mid Skilled Workers (per capita)'),
+# cbind('sia_nprod_med_skill_pvt_pcapita','Private Outpatient procedures by Mid Skilled Workers (per capita)'),
+# 
+# cbind('sia_nprod_high_skill_pcapita','Outpatient Procedures by High Skilled Workers (per capita)'),
+# cbind('sia_nprod_high_skill_mun_pcapita','Municipal Outpatient Procedures by High Skilled Workers (per capita)'),
+# cbind('sia_nprod_high_skill_pub_pcapita','Public Outpatient Procedures by High Skilled Workers (per capita)'),
+# cbind('sia_nprod_high_skill_pvt_pcapita','Private Outpatient Procedures by High Skilled Workers (per capita)'),
+# 
+# cbind('sia_ncnes_amb_pcapita','N. of Health Facilities with Ambulatory Service (per capita*1000)'),
+# cbind('sia_ncnes_amb_pub_pcapita','N. of Public Health Facilities with Ambulatory Service (per capita*1000)'),
+# cbind('sia_ncnes_amb_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service (per capita*1000)'),
+# 
+# cbind('sia_ncnes_amb_lc_pcapita','N. of Health Facilities with Low & Mid Complexity Ambulatory Service (per capita*1000)'),
+# cbind('sia_ncnes_amb_lc_pub_pcapita','N. of Public Health Facilities with Low & Mid Complexity Ambulatory Service (per capita*1000)'),
+# cbind('sia_ncnes_amb_lc_pvt_pcapita','N. of Private Health Facilities with Low & Mid Complexity Ambulatory Service (per capita*1000)'),
+# 
+# cbind('sia_ncnes_amb_hc_pcapita','N. of Health Facilities with High Complexity Ambulatory Service (per capita*1000)'),
+# cbind('sia_ncnes_amb_hc_pub_pcapita','N. of Public Health Facilities with High Complexity Ambulatory Service (per capita*1000)'),
+# cbind('sia_ncnes_amb_hc_pvt_pcapita','N. of Private Health Facilities with High Complexity Ambulatory Service (per capita*1000)'),
+# 
+# 
+# cbind('sia_ncnes_low_skill_pcapita','N. of Health Facilities with Ambulatory Service by Low Skilled Workers (per capita*1000)'),
+# cbind('sia_ncnes_low_skill_pub_pcapita','N. of Public Health Facilities with Ambulatory Service by Low Skilled Workers (per capita*1000)'),
+# cbind('sia_ncnes_low_skill_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service by Low Skilled Workers (per capita*1000)'),
+# 
+# cbind('sia_ncnes_med_skill_pcapita','N. of Health Facilities with Ambulatory Service by Mid Skilled Workers (per capita*1000)'),
+# cbind('sia_ncnes_med_skill_pub_pcapita','N. of Public Health Facilities with Ambulatory Service by Mid Skilled Workers (per capita*1000)'),
+# cbind('sia_ncnes_med_skill_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service by Mid Skilled Workers (per capita*1000)'),
+# 
+# cbind('sia_ncnes_high_skill_pcapita','N. of Health Facilities with Ambulatory Service by High Skilled Workers (per capita*1000)'),
+# cbind('sia_ncnes_high_skill_mun_pcapita','N. of Municipal Health Facilities with Ambulatory Service by High Skilled Workers (per capita*1000)'),
+# cbind('sia_ncnes_high_skill_pub_pcapita','N. of Public Health Facilities with Ambulatory Service by High Skilled Workers (per capita*1000)'),
+# cbind('sia_ncnes_high_skill_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service by High Skilled Workers (per capita*1000)'),
+# 
+# cbind('sia_ncnes_enf_pcapita','N. of Health Facilities with Ambulatory Service by Nurses (per capita*1000)'),
+# cbind('sia_ncnes_enf_pub_pcapita','N. of Public Health Facilities with Ambulatory Service and with Nurses (per capita*1000)'),
+# cbind('sia_ncnes_enf_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service and with Nurses (per capita*1000)'),
+# 
+# cbind('sia_ncnes_enfobs_pcapita','N. of Health Facilities with Ambulatory Service by Obstetrical Nurses (per capita*1000)'),
+# cbind('sia_ncnes_enfobs_pub_pcapita','N. of Public Health Facilities with Ambulatory Service by Obstetrical Nurses (per capita*1000)'),
+# cbind('sia_ncnes_enfobs_pvt_pcapita','N. of Private Health Facilities with Ambulatory Service by Obstetrical Nurses (per capita*1000)'),
+# 
+# cbind('sia_ncnes_ginobs_pcapita','N. of Health Facilities with Obstetrical/Gyneco. Ambulatory Service (per capita*1000)'),
+# cbind('sia_ncnes_ginobs_pub_pcapita','N. of Public Health Facilities with Obstetrical/Gyneco. Ambulatory Service (per capita*1000)'),
+# cbind('sia_ncnes_ginobs_pvt_pcapita','N. of Private Health Facilities with Obstetrical/Gyneco. Ambulatory Service (per capita*1000)'),
+# 
+# cbind('sia_ncnes_pediat_pcapita','N. of Health Facilities with Pediatric Ambulatory Service (per capita*1000)'),
+# cbind('sia_ncnes_pediat_pub_pcapita','N. of Public Health Facilities with Pediatric Ambulatory Service (per capita*1000)'),
+# cbind('sia_ncnes_pediat_pvt_pcapita','N. of Private Health Facilities with Pediatric Ambulatory Service (per capita*1000)'),
+# 
+# 
+# 
+# 
+# 
+# )
 
 
 
@@ -145,7 +145,7 @@ var_map <- rbind(cbind('sia_pcapita','N. Outpatient Procedures (per capita)'),
 # 3. Run and ouput
 # =================================================================
 
-for (i in seq(1,6,1)){
+for (i in seq(1,4,1)){
   var <- var_map[i,1]
   var_name <- var_map[i,2]
   print(var_name)
