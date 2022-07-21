ba08b8b866aa7887a488e9b99f7d6b0aa00905de
michelszklo <mszklo@gmail.com>
2022-07-21 15:28:20 -0300

    Create SIH regs

---
+++ 19_regs_sih.R
@@ -0,0 +1,116 @@
+#######################################################################################################
+# Author: Michel Szklo
+# April 2021
+# 
+# This scripts runs regressions for hospitalization
+#
+#
+#######################################################################################################
+
+# 0. Set-up
+# =================================================================
+
+rm(list=ls())
+
+# packages
+packages<-c('readr',
+            'tidyverse',
+            'dplyr',
+            'RCurl',
+            'tidyr',
+            'scales',
+            'RColorBrewer',
+            'ggplot2',
+            'xlsx',
+            'stringdist',
+            'textclean',
+            'readstata13',
+            'lfe',
+            'fastDummies',
+            'purrr',
+            'boot',
+            'broom',
+            'modelsummary',
+            'ggsci')
+to_install<-packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(to_install)>0) install.packages(to_install)
+
+lapply(packages,require,character.only=TRUE)
+
+
+options(digits = 15)
+
+# SET PATH FOR EC 29-2000 ON YOUR COMPUTER
+# ------------------------------------
+
+dir <- "C:/Users/Michel/Google Drive/DOUTORADO FGV/Artigos/EC 29-2000/"
+
+# ------------------------------------
+
+
+# 1. Load data
+# =================================================================
+load(paste0(dir,"regs.RData"))
+
+# 2. Define outcomes output name and output functions
+# =================================================================
+
+var_map <- rbind(cbind('tx_sih_infant','Infant Hospitalization Rate (pop 0-1y * 1000)'),
+                 cbind('tx_sih_infant_icsap','Infant Hospitalization Rate - APC (pop 0-1y * 1000)'),
+                 cbind('tx_sih_infant_nicsap','Infant Hospitalization Rate - non-APC (pop 0-1y * 1000)'),
+                 cbind('tx_sih_maternal','Maternal Hospitalization Rate (women 10-49y * 1000)')
+   )
+
+
+
+# 3. Run and ouput
+# =================================================================
+
+for (i in seq(1,3,1)){
+  var <- var_map[i,1]
+  var_name <- var_map[i,2]
+  print(var_name)
+  
+  regress_output(var,var_name,3,1998,"peso_b")
+  
+  
+  if(exists("df_table_all")){
+    df_table_all <- rbind(df_table_all,table_all)
+    
+  } else {
+    
+    df_table_all <- table_all
+    
+  }
+  
+}
+
+for (i in seq(4,4,1)){
+  var <- var_map[i,1]
+  var_name <- var_map[i,2]
+  print(var_name)
+  
+  regress_output(var,var_name,3,1998,"peso_m")
+  
+  
+  if(exists("df_table_all")){
+    df_table_all <- rbind(df_table_all,table_all)
+    
+  } else {
+    
+    df_table_all <- table_all
+    
+  }
+  
+}
+
+
+
+
+# exporting results
+# ---------------------
+
+write.xlsx2(df_table_all, file = paste0(dir,main_folder,output_file),sheetName = "hosp",row.names = F,append = T)
+
+
+
