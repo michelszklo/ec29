eb3dcd2bca1bcaae6f77d85daf371ff4e7c764c0
Michel Szklo <36962976+michelszklo@users.noreply.github.com>
2025-03-25 16:59:56 -0300

    updates rais script

--- rais.R
+++ rais.R
@@ -3,7 +3,6 @@
 # June 2024
 # 
 # This scripts downloads other economic data for robustness checks:
-#     - education: number of students
 #     - labor: number of formal workers
 #
 #
@@ -55,27 +54,44 @@ df_dict <- bd_collect(query_dict)
 
 
 
-# 1. RAIS
+# 1. RAIS download and collapse
 # =================================================================
 # 
-query <- bdplyr("br_me_rais.microdados_vinculos")
+query <- bdplyr("br_me_rais.microdados_estabelecimentos")
 
 
 for (i in seq.int(1996,2010)){
   print(i)
   q_i <- query %>%
     dplyr::filter(ano==i) %>%
-    dplyr::select(ano,sigla_uf,id_municipio,tipo_vinculo,
-                  vinculo_ativo_3112,tempo_emprego,quantidade_horas_contratadas)
+    dplyr::select(ano,sigla_uf,id_municipio,quantidade_vinculos_ativos)
   
   df <- bd_collect(q_i)
-  print("downloaded")
-  df <- df %>% 
-    group_by(ano,sigla_uf,id_municipio,tipo_vinculo,vinculo_ativo_3112) %>% 
-    summarise(tempo_emprego = mean(tempo_emprego, na.rm = T),
-              quantidade_horas_contratadas = mean(quantidade_horas_contratadas, na.rm = T))
+  print(paste0(i," downloaded"))
   
   saveRDS(df,file=paste0(dir,"data/RAIS/RAIS",i,".Rds"))
-  print("saved")
+  print(paste0(i," saved"))
 
 }
+
+
+# 2. RAIS consol
+# =================================================================
+
+path <- paste0(dir,"data/RAIS/")
+files <- list.files(path, pattern = "\\.Rds$", full.names = TRUE)
+
+rais <- lapply(files, readRDS)
+
+rais <- do.call(rbind, rais)
+rais <- rais %>% 
+  group_by(ano,sigla_uf,id_municipio) %>% 
+  summarise(rais_employees = sum(quantidade_vinculos_ativos,rm.na = T),
+            rais_employeers = n()) %>% 
+  ungroup() %>% 
+  rename(cod_mun = id_municipio) %>% 
+  mutate(cod_mun = as.numeric(substr(cod_mun,1,6)))
+
+saveRDS(rais,file = paste0(dir,"data/RAIS/rais_consol.rds"))
+write.csv(rais, file = paste0(dir,"data/RAIS/rais_consol.csv"),row.names = T)
+
